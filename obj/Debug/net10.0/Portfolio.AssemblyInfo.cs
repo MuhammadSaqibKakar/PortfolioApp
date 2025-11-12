cd3ec05e4dcc119216657018ffc477551714da40
MuhammadSaqibKakar <engrsaqibkakar241@gmail.com>
2025-11-12 15:29:49 +0500

    Initial commit: complete To-Do List / Portfolio project with full UI and working images

--- obj/Debug/net10.0/Portfolio.AssemblyInfo.cs
+++ obj/Debug/net10.0/Portfolio.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+baaa315ebfbfa14c6b550227d29508160853d2ed")]
 [assembly: System.Reflection.AssemblyProductAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
